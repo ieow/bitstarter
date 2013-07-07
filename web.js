d45a1d53cbb967e46d3404d9b0e7fbeab8af07b7
ubuntu <ubuntu@ubuntu-desktop.(none)>
2013-07-07 14:23:03 +0800

    update

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express()
 
+app.configure( function() {
+	app.use(express.logger());
+
+	app.set('views', __dirname + '/client/views');
+	app.set("view options", {layout: false});
+
+	app.use(express.static(__dirname + '/client'));
+
+	//app.engine('html' , require('jade').__express);
+});
+
+
+
+
+
+//
+// app.get('/', function(request, response) {
+//   response.send('Hello World 2!');
+// });
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//	response.render('index.html');
+	response.sendfile('client/index.html');
 });
 
 var port = process.env.PORT || 5000;
